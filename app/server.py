b42df146809e22a94d46f03e0245988803369b62
Mikful <40219831+Mikful@users.noreply.github.com>
2020-08-12 17:33:46 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -19,7 +19,6 @@ from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://docs.google.com/uc?export=download'
 id = '1-5gNLy4KpWEAxM7OYfyri8YbtifBRvv7'
-response = session.get(URL, params = { 'id' : id }, stream = True)
 #export_file_url = "https://www.googleapis.com/drive/v3/files/1-5gNLy4KpWEAxM7OYfyri8YbtifBRvv7/?key=AIzaSyCMqdjFMjVUCy1VhUznkaJcUy89pSFURFk&alt=media"
 export_file_name = 'learner.pkl'
 
