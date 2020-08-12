51968eaedbdf67758c26f326e405b6bc3a34c7b8
Mikful <40219831+Mikful@users.noreply.github.com>
2020-08-12 15:39:37 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -17,7 +17,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1-5gNLy4KpWEAxM7OYfyri8YbtifBRvv7'
+export_file_url = 'https://drive.google.com/uc?export=download&amp;confirm=UC0Z&amp;id=1-5gNLy4KpWEAxM7OYfyri8YbtifBRvv7'
 export_file_name = 'learner.pkl'
 
 with open('app/classes.txt', 'r') as f:
