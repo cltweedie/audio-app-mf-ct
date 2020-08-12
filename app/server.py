2986f3cf64e2cb2ad88b81b4efffdcceaa89094e
Mikful <40219831+Mikful@users.noreply.github.com>
2020-08-12 15:10:24 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,8 +1,6 @@
 # import os # for heroku only
 # import requests # for heroku only
 # Port = int(os.environ.get('PORT', 50000)) # for heroku only
-# import urllib.request # new attempt at Google Drive
-# import shutil # new attempt at Google Drive
 import aiohttp
 import asyncio
 import uvicorn
@@ -17,11 +15,9 @@ from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
-from pydrive.auth import GoogleAuth # new attempt at google drive download
 
 
 export_file_url = 'https://drive.google.com/uc?export=download&id=1-5gNLy4KpWEAxM7OYfyri8YbtifBRvv7'
-export_file_id = "1-5gNLy4KpWEAxM7OYfyri8YbtifBRvv7"
 export_file_name = 'learner.pkl'
 
 with open('app/classes.txt', 'r') as f:
@@ -32,34 +28,23 @@ app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
-## original code
-# async def download_file(url, dest):
-#     if dest.exists(): return
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url) as response:
-#             data = await response.read()
-#             with open(dest, 'wb') as f:
-#                 f.write(data)
 
-
-# new code Google Drive Download
-async def download_file(id, dest):
-    if dest.exists(): return "dest.exists() True"
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
-    drive = GoogleDrive(gauth)
-    file_obj = drive.CreateFile({'id': id})
-    file_obj.GetContentFile(dest)
-
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
 
 
 async def setup_learner():
-    #await download_file(export_file_url, path/export_file_name)
-    await download_file(export_file_id, path/export_file_name) # google drive
+    await download_file(export_file_url, path/export_file_name)
     try:
         def get_file(r): return '../content/train_curated/'+r['fname'] # to avoid AttributeError
         def get_label(r): return r['labels'].split(',') # to avoid AttributeError
         print("pkl file exists?:", path/export_file_name, os.path.exists(path/export_file_name))
+        print("dl pkl file size:", Path(path/export_file_name).stat().st_size)
         learn = load_learner(path/export_file_name)
         return learn
     except RuntimeError as e:
